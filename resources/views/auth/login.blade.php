dd853b93acc95755dafa7e6ee216707f563dc491
emon21 <emonbd66@gmail.com>
2023-12-31 12:48:06 +0600

    chore:Multi auth useby middleware

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,6 +16,9 @@
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                @if (session('error'))
+                                <strong class="text-danger">{{ session('error') }}</strong>
+                                 @endif
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -36,6 +39,7 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
+
                             </div>
                         </div>
 
